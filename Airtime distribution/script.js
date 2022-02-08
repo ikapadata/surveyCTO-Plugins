00836f5b5be3f8f0fbd50a6b60cab89c4bda09c2
ikapadata <46567217+ikapadata@users.noreply.github.com>
2022-02-08 09:38:17 +0200

    airtime script
    send airtime to phone numbers through reloadly

---
+++ Airtime distribution/script.js
@@ -0,0 +1,82 @@
+// Define global variables
+var hash_parameter = getPluginParameter("hash");
+var category = getPluginParameter("type");
+var amount = getPluginParameter("amount");
+var networkCell = getPluginParameter("network");
+var reloadlyId = getPluginParameter("reloadlyId");
+var reloadlySecret = getPluginParameter("reloadlySecret");
+var phone = getPluginParameter("phone");
+var incentive_reference = getPluginParameter("reference");
+var project = getPluginParameter("project");
+var ip_address ="";
+var isoName =  "ZA";//getPluginParameter("isoName");
+var URL =""
+
+reqData = {"Network":networkCell,
+          "Amount": amount ,
+          "Cellphone":phone,
+          "Category": category,
+          "Incentive_Reference": incentive_reference,
+          "Project": project}
+
+
+// This is a basic hash function to convert a string to 32bit integer.
+function stringToHash(string) {
+  var hash = 0;
+  if (string.length == 0) return hash;
+  for (i = 0; i < string.length; i++) {
+      char = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash = hash & hash;
+  }
+  return hash;
+}
+
+// Define what happens once the IP address has been obtained.
+function finishedJsonLoad() {
+  $(".loading-container").hide(); // Hide the loading section.
+  // Check whether the IP address should be hashed (based on the parameter).
+  if(hash_parameter=="yes") {
+    var hashed_ip = stringToHash(ip_address); // Hash the collected IP address.
+    setAnswer(hashed_ip); // Store the hashed IP address as the field's response.
+  }
+  else {
+    setAnswer(ip_address); // Store the IP address as the field's response.
+  }
+  goToNextField(); // Go to next field once ip_address is set.
+}
+
+// Use JQuery to get IP address from ipify.org
+$.get("https://api.ipify.org", function(e) {
+  ip_address = e; // Assign returned value to ip_address.
+  finishedJsonLoad(); // Once we have obtined the IP address, process the result.
+});
+
+/*
+$.post(URL,
+      reqData,
+      function(data){
+        ip_address =  data;
+        console.log(ip_address);
+        finishedJsonLoad()
+      }
+    );
+*/
+
+//// Shown below is code that is not specific to this field plug-in. It contains general helpers and functions for handling fields in SurveyCTO Collect.
+
+// If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < anmd > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
+function unEntity(str){
+  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+}
+if (fieldProperties.LABEL) {
+  document.querySelector(".label").innerHTML = unEntity(fieldProperties.LABEL);
+}
+if (fieldProperties.HINT) {
+  document.querySelector(".hint").innerHTML = unEntity(fieldProperties.HINT);
+}
+
+// Define what happens when the user attempts to clear the response.
+function clearAnswer() {
+  setAnswer("");
+}
